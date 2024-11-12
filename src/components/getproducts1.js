01261d0dd85fe481e0f19bebeda1610cf33201ea
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2024-11-12 16:16:53 +0530

    added css

--- src/components/getproducts1.js
+++ src/components/getproducts1.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import heropic from './hero.jpg'
 
 
 function Category() {
@@ -38,13 +39,13 @@ function Category() {
 
 
     return (
-        <div>
-            <div>
+        <div style={{  backgroundColor:'#bbe9db'}}>
+            <div style={{paddingBottom:'25px',backgroundColor:'#79c2d0'}}>
                 {categories.map((category, index) => (
-                    <button 
-                        style={{ marginLeft: '200px', marginTop: '25px', textTransform: 'uppercase' }}
+                   <button 
+                        style={{color:'black', backgroundColor:'#5585b5', height:'40px',width:'200px',marginLeft: '150px ', marginTop: '25px', textTransform: 'uppercase' }}
                         key={index} 
-                        className="btn btn-secondary" 
+                        className="btn " 
                         onClick={() => handleButtonClick(category)}
                     >
                         {category}
@@ -52,24 +53,33 @@ function Category() {
                 ))}
             </div>
 
-            <div className="container text-center" style={{ marginTop: '50px' }}>
+            <div className="container text-center" style={{  backgroundColor:'FFCBB6s',padding: '50px 0' }}>
+
+            <div class="card bg-dark text-white" style={{height:'200px'}}>
+                    <img class="card-img" src={heropic} alt="Card image" style={{height:'200px'}}/>
+                    <div class="card-img-overlay">
+                    
+                    </div>
+            </div>
+
+
                 {items.map((item) => (
                     <div className="d-inline-flex p-2">
                         <Link to={`/product/${item.id}`} style={{textDecoration:'none'}}>
-                        <div className="card"        style={{ alignItems: 'center', width: '250px', height: '500px', overflow: 'hidden' }}>
+                        <div className="card"        style={{ boxShadow:'0 2px 4px black',backgroundColor:'#9ba6a5', alignItems: 'center', width: '250px', height: '500px', overflow: 'hidden' }}>
                             <img className="card-img-top" src={item.image} alt={item.title}       style={{ marginTop: '20px', width: '150px', height: '150px' }} />
                             <div className="card-body">
                                 <h6 className="card-title">{item.title}</h6>
                                 <p style={{fontSize: '15px' }}>Price: {item.price}$ <br /> Rated {item.rating.rate}</p>
                                 <a href="#" className="btn btn-primary">
 
-                                    <svg style={{ marginRight: '5px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                                    <svg style={{ marginRight: '5px'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                                     </svg>
                                     
                                     Add to cart
                                 </a>
-                                <p className="card-text" style={{ fontSize: '10px' }}>{item.description}</p>
+                                <p className="card-text" style={{ fontSize: '10px',marginTop:'15px'  }}>{item.description}</p>
                             </div>
                         </div>
                         </Link>
