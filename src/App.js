8a412923cf307c13786ee505852bf059b7fea92b
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2024-11-12 19:13:13 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route,Routes, BrowserRouter } from 'react-router-dom';
+import Category from './components/getproducts1';
+import ProductDetail from './components/singleitem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes, BrowserRouter } from 'react-router-dom';
 import Category from './components/getproducts1';
 import ProductDetail from './components/singleitem';
 
